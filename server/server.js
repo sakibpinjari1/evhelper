2ccf55a9ece6b12ed13b0708c45b1fe11a8039be
Sakib Pinjari <sakib@example.com>
2026-01-18 14:10:23 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import http from "http";
+import app from "./src/app.js";
+import { Server } from "socket.io";
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+});
+
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
